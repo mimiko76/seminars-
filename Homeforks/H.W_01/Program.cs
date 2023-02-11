4c1fcf78eafc706a9ad31ff349740f97308e3e24
artem_levin <tema.levin2016@gmail.com>
2023-02-11 10:30:29 +0300

    first task

---
+++ Homeforks/H.W_01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Console.WriteLine("Введите первое число:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int min, max;
+// if (num1 > num2)
+// {
+//    max = num1;
+//    min = num2;
+//    Console.WriteLine($"Число {max} больше чем {min}");
+// }
+// else
+// {
+//    max = num2;
+//    min = num1;
+//    Console.WriteLine($"Число {max} больше чем {min}");
+// }
