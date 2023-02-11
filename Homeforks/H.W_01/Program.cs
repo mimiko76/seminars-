8df4b2dacff82be964c48fc66da43762931f7245
artem_levin <tema.levin2016@gmail.com>
2023-02-11 11:14:56 +0300

    second task

--- Homeforks/H.W_01/Program.cs
+++ Homeforks/H.W_01/Program.cs
@@ -1,8 +1,10 @@
-﻿// Console.WriteLine("Введите первое число:");
+﻿//______________Задача 1(2)_________
+// Console.WriteLine("Введите первое число:");
 // int num1 = Convert.ToInt32(Console.ReadLine());
 
 // Console.WriteLine("Введите второе число:");
 // int num2 = Convert.ToInt32(Console.ReadLine());
+
 // int min, max;
 // if (num1 > num2)
 // {
@@ -16,3 +18,29 @@
 //    min = num1;
 //    Console.WriteLine($"Число {max} больше чем {min}");
 // }
+//_____________Задача 2 (4)___________
+// int[] num = new int[3];
+// int count, max;
+// count = 0;
+// Console.WriteLine("Введите три числа:");
+// while (count <= 2)
+// {
+//    num[count] = Convert.ToInt32(Console.ReadLine());
+//    count++;
+// }
+// count = 0;
+// max = num[0];
+// while (count <= 2)
+// {
+//    if (max < num[count])
+//    {
+//       max = num[count];
+
+//    }
+//    else
+//    {
+//       max = max;
+//    }
+//    count++;
+// }
+// Console.WriteLine($"Ваше максимальное число - {max}");
